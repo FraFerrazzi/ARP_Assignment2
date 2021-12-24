0abc8d675df26fccc564d245dd715e5b32945347
FraFerrazzi <91314392+FraFerrazzi@users.noreply.github.com>
2021-12-24 13:02:06 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+unzip sources.zip
+mv sources $1
+
+cd /$1/unnamedpipe
+gcc ProCliUnamedPipe.c -o ProCliUnamedPipe
+
+cd /$1/namedpipe
+gcc ProducerNamedPipe.c -o ProducerNamedPipe
+gcc ClientNamedPipe.c -o ClientNamedPipe
+
+cd /$1/socket
+gcc ServerSocket.c -o ServerSocket
+gcc ClientSocket.c -o ClientSocket
+
+cd /$1/circularbuffer
+gcc CircularBufferProducer.c -o CircularBufferProducer
+gcc CircularBufferClient.c -o CircularBufferClient
+
+
---
+++ running.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+cd /$1
+
+read -sp 'Command: ' user
+echo
+
+if [ $user -eq 1 ]
+then
+	cd /$1/unnamedpipe
+	echo Launching unnamed pipe for data transferring 
+	./ProCliUnamedPipe
+	cd ..
+	sleep 1
+
+elif [ $user -eq 2 ]
+then
+	cd /$1/namedpipe
+	echo Launching named pipe for data transferring 
+	./ProducerNamedPipe
+	cd ..
+	sleep 1
+
+elif [ $user -eq 3 ]
+then
+	cd /$1/socket
+	> socketdata.txt
+	read -sp 'Decide the amount of random Data that will be generated in Mb from 1 to 100: ' datasizeso
+	if [ $datasizeso -gt 0 ] && [ $datasizeso -lt 101 ]
+	then
+		echo $datasizeso >> socketdata.txt
+		echo
+		echo Launching socket for data transferring 
+		source runSocket.sh
+		cat socketdata.txt
+		sleep 1
+	else
+		echo
+		echo ERROR!! Not the right input! Please choose an integer number between 1 and 100
+		sleep 1
+	fi
+	cd ..
+
+elif [ $user -eq 4 ]
+then
+	cd /$1/circularbuffer
+	> circularbufferdata.txt
+	read -sp 'Decide the amount of random Data that will be generated in Mb from 1 to 100: ' datasizecb
+	if [ $datasizecb -gt 0 ] && [ $datasizecb -lt 101 ]
+	then
+		echo $datasizecb >> circularbufferdata.txt
+		echo
+		echo Launching circular buffer for data transferring 
+		source runCircularBuffer.sh
+		cat circularbufferdata.txt
+		sleep 1
+	else
+		echo
+		echo ERROR!! Not the right input! Please choose an integer number between 1 and 100
+		sleep 1
+	fi
+	cd ..
+
+else 
+	echo Not a correct input. Please choose a number between 1 and 4.
+	echo Type 1 for unnamed pipe
+	echo Type 2 for named pipe
+	echo Type 3 for socket
+	echo Type 4 for circular buffer
+	sleep 1
+fi
+
+
